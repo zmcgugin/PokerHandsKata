e5d04713ed3e44a4a6dbeb55df3b4ff205c751d8
zmcgugin <ZMcGugin@contractor.lb.com>
2019-06-12 12:13:44 -0400

    adding test for high card

--- poker/poker_winner.py
+++ poker/poker_winner.py
@@ -1,5 +1,40 @@
 
 
+
 class PokerWinner:
+    card_values = {
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8,
+        '9': 9,
+        '10': 10,
+        'J': 11,
+        'Q': 12,
+        'K': 13,
+        'A': 14
+    }
+
     def find_winning_hand(hands):
-        return 'Zak'
+        return PokerWinner.high_card(hands)[0]
+
+    def high_card(hands):
+        max_hand = None
+        max_hand_value = 0
+        for hand in hands:
+            max_card_value = 0
+            for card in hand[1]:
+                current_card_value = PokerWinner.card_values[card[:-1]]
+                if current_card_value > max_card_value:
+                    max_card_value = current_card_value
+
+            if max_card_value > max_hand_value:
+                max_hand_value = max_card_value
+                max_hand = hand
+
+        return max_hand
+
+
--- test/poker_winner_test.py
+++ test/poker_winner_test.py
@@ -8,3 +8,8 @@ class MyTest(unittest.TestCase):
         zaks_hand = ('Zak', ['AS', '2H', '3H', 'JC', '8D'])
         jons_hand = ('Jon', ['KS', '2H', '3H', 'JC', '8D'])
         self.assertEqual(PokerWinner.find_winning_hand([zaks_hand, jons_hand]), 'Zak')
+
+    def test_high_card_2(self):
+        zaks_hand = ('Zak', ['KS', '2H', '3H', 'JC', '8D'])
+        jons_hand = ('Jon', ['6S', '2H', '3H', 'JC', 'AS'])
+        self.assertEqual(PokerWinner.find_winning_hand([zaks_hand, jons_hand]), 'Jon')
