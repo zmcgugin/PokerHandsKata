e58dd7bb6ef52ecdaebbb923460d0514a24ad964
zmcgugin <ZMcGugin@contractor.lb.com>
2019-06-14 14:09:48 -0400

    refactoring and removing needless class

--- poker/poker_winner.py
+++ poker/poker_winner.py
@@ -1,208 +1,205 @@
 from poker.result import *
 import functools
 
+card_values = {
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+    'J': 11,
+    'Q': 12,
+    'K': 13,
+    'A': 14
+}
 
-class PokerWinner:
+hand_value = {
+    'high_card': 1,
+    'pair': 2,
+    'two_pair': 3,
+    'three_of_a_kind': 4,
+    'straight': 5,
+    'flush': 6,
+    'full_house': 7,
+    'four_of_a_kind': 8,
+    'straight_flush': 9
+}
 
-    card_values = {
-        '2': 2,
-        '3': 3,
-        '4': 4,
-        '5': 5,
-        '6': 6,
-        '7': 7,
-        '8': 8,
-        '9': 9,
-        '10': 10,
-        'J': 11,
-        'Q': 12,
-        'K': 13,
-        'A': 14
-    }
+def find_winning_hand(hands):
+    results = list(map(parse_hands, hands))
+    return sorted(results, key=functools.cmp_to_key(sort))[0].hand[0]
 
-    hand_value = {
-        'high_card': 1,
-        'pair': 2,
-        'two_pair': 3,
-        'three_of_a_kind': 4,
-        'straight': 5,
-        'flush': 6,
-        'full_house': 7,
-        'four_of_a_kind': 8,
-        'straight_flush': 9
-    }
 
-    def find_winning_hand(self, hands):
-        results = list(map(PokerWinner.parse_hands, hands))
-        return sorted(results, key=functools.cmp_to_key(PokerWinner.sort))[0].hand[0]
+def compare_tie_breaker(a, b, index=0):
+    if a[index] == b[index]:
+        return compare_tie_breaker(a, b, index + 1)
+    else:
+        return a[index] > b[index]
 
-    @staticmethod
-    def compare_tie_breaker(a, b, index=0):
-        if a[index] == b[index]:
-            return PokerWinner.compare_tie_breaker(a, b, index + 1)
-        else:
-            return a[index] > b[index]
 
-    @staticmethod
-    def parse_hands(hand):
-        result = PokerWinner.straight_flush(hand)
-        if result is None:
-            result = PokerWinner.four_of_a_kind(hand)
-        if result is None:
-            result = PokerWinner.full_house(hand)
-        if result is None:
-            result = PokerWinner.flush(hand)
-        if result is None:
-            result = PokerWinner.straight(hand)
-        if result is None:
-            result = PokerWinner.three_of_a_kind(hand)
-        if result is None:
-            result = PokerWinner.two_pair(hand)
-        if result is None:
-            result = PokerWinner.pair(hand)
-        if result is None:
-            result = PokerWinner.high_card(hand)
-        return result
+def parse_hands(hand):
+    result = straight_flush(hand)
+    if result is None:
+        result = four_of_a_kind(hand)
+    if result is None:
+        result = full_house(hand)
+    if result is None:
+        result = flush(hand)
+    if result is None:
+        result = straight(hand)
+    if result is None:
+        result = three_of_a_kind(hand)
+    if result is None:
+        result = two_pair(hand)
+    if result is None:
+        result = pair(hand)
+    if result is None:
+        result = high_card(hand)
+    return result
 
-    @staticmethod
-    def high_card(hand):
-        cards = PokerWinner.get_cards_number_values(hand)
-        tie_breaker = sorted(cards, reverse=True)
-        return Result('high_card', hand, tie_breaker)
 
-    @staticmethod
-    def pair(hand):
-        hand_without_suits = PokerWinner.get_cards_number_values(hand)
-        pair = PokerWinner.find_duplicates(hand_without_suits, 2)
+def high_card(hand):
+    cards = get_cards_number_values(hand)
+    tie_breaker = sorted(cards, reverse=True)
+    return Result('high_card', hand, tie_breaker)
 
-        if pair:
-            tie_breakers = PokerWinner.create_tie_breaker_list(pair, hand_without_suits)
-            return Result('pair', hand, tie_breakers)
-        else:
-            return None
 
-    @staticmethod
-    def two_pair(hand):
-        hand_without_suits = PokerWinner.get_cards_number_values(hand)
-        pairs = PokerWinner.find_duplicates(hand_without_suits, 2)
+def pair(hand):
+    hand_without_suits = get_cards_number_values(hand)
+    pair = find_duplicates(hand_without_suits, 2)
 
-        if pairs and len(pairs) == 2:
-            tie_breakers = PokerWinner.create_tie_breaker_list(pairs, hand_without_suits)
-            return Result('two_pair', hand, tie_breakers)
-        else:
-            return None
+    if pair:
+        tie_breakers = create_tie_breaker_list(pair, hand_without_suits)
+        return Result('pair', hand, tie_breakers)
+    else:
+        return None
 
-    @staticmethod
-    def three_of_a_kind(hand):
-        hand_without_suits = PokerWinner.get_cards_number_values(hand)
-        trips = PokerWinner.find_duplicates(hand_without_suits, 3)
 
-        if trips:
-            tie_breakers = PokerWinner.create_tie_breaker_list(trips, hand_without_suits)
-            return Result('three_of_a_kind', hand, tie_breakers)
-        else:
-            return None
+def two_pair(hand):
+    hand_without_suits = get_cards_number_values(hand)
+    pairs = find_duplicates(hand_without_suits, 2)
 
-    @staticmethod
-    def straight(hand):
-        hws = sorted(PokerWinner.get_cards_number_values(hand))
+    if pairs and len(pairs) == 2:
+        tie_breakers = create_tie_breaker_list(pairs, hand_without_suits)
+        return Result('two_pair', hand, tie_breakers)
+    else:
+        return None
 
-        if PokerWinner.is_straight(hws):
-            # using number in middle of straight as a tie breaker to avoid low ace's
-            return Result('straight', hand, [hws[2]])
-        else:
-            return None
 
-    @staticmethod
-    def flush(hand):
-        suits = sorted(PokerWinner.get_cards_suits(hand))
+def three_of_a_kind(hand):
+    hand_without_suits = get_cards_number_values(hand)
+    trips = find_duplicates(hand_without_suits, 3)
 
-        if PokerWinner.is_flush(suits):
-            return Result('flush', hand, sorted(PokerWinner.get_cards_number_values(hand), reverse=True))
-        else:
-            return None
+    if trips:
+        tie_breakers = create_tie_breaker_list(trips, hand_without_suits)
+        return Result('three_of_a_kind', hand, tie_breakers)
+    else:
+        return None
 
-    @staticmethod
-    def full_house(hand):
-        hand_without_suits = PokerWinner.get_cards_number_values(hand)
-        pair = PokerWinner.find_duplicates(hand_without_suits, 2)
-        trips = PokerWinner.find_duplicates(hand_without_suits, 3)
 
-        if pair and trips:
-            return Result('full_house', hand, [trips[0], pair[0]])
-        else:
-            return None
+def straight(hand):
+    hws = sorted(get_cards_number_values(hand))
 
-    @staticmethod
-    def four_of_a_kind(hand):
-        hand_without_suits = PokerWinner.get_cards_number_values(hand)
-        quads = PokerWinner.find_duplicates(hand_without_suits, 4)
+    if is_straight(hws):
+        # using number in middle of straight as a tie breaker to avoid low ace's
+        return Result('straight', hand, [hws[2]])
+    else:
+        return None
 
-        if quads:
-            tie_breakers = PokerWinner.create_tie_breaker_list(quads, hand_without_suits)
-            return Result('four_of_a_kind', hand, tie_breakers)
-        else:
-            return None
 
-    @staticmethod
-    def straight_flush(hand):
-        hws = sorted(PokerWinner.get_cards_number_values(hand))
-        suits = sorted(PokerWinner.get_cards_suits(hand))
+def flush(hand):
+    suits = sorted(get_cards_suits(hand))
 
-        if PokerWinner.is_straight(hws) and PokerWinner.is_flush(suits):
-            # using number in middle of straight as a tie breaker to avoid low ace's
-            return Result('straight_flush', hand, [hws[2]])
-        else:
-            return None
+    if is_flush(suits):
+        return Result('flush', hand, sorted(get_cards_number_values(hand), reverse=True))
+    else:
+        return None
 
-    @staticmethod
-    def create_tie_breaker_list(main_hand, full_hand):
-        main_hand.sort(reverse=True)
-        kickers = [x for x in full_hand if x not in main_hand]
-        kickers.sort(reverse=True)
-        main_hand.extend(kickers)
-        return main_hand
 
-    @staticmethod
-    def find_duplicates(numbers, number_of_dups):
-        duplicateNumbers = []
-        for x in PokerWinner.card_values.values():
-            duplicateCount = numbers.count(x)
-            if duplicateCount == number_of_dups:
-                duplicateNumbers.append(x)
-        return duplicateNumbers
+def full_house(hand):
+    hand_without_suits = get_cards_number_values(hand)
+    pair = find_duplicates(hand_without_suits, 2)
+    trips = find_duplicates(hand_without_suits, 3)
 
-    @staticmethod
-    def get_cards_number_values(hand):
-        return list(map(lambda x: PokerWinner.card_values[x[:-1]], hand[1]))
+    if pair and trips:
+        return Result('full_house', hand, [trips[0], pair[0]])
+    else:
+        return None
 
-    @staticmethod
-    def get_cards_suits(hand):
-        return list(map(lambda x: x[len(x) - 1], hand[1]))
 
-    @staticmethod
-    def is_straight(hws):
-        low_ace_straight = hws[0] + 1 == hws[1] and hws[1] + 1 == hws[2] and hws[2] + 1 == hws[3] and hws[4] == 14
-        straight = hws[0] + 1 == hws[1] and hws[1] + 1 == hws[2] and hws[2] + 1 == hws[3] and hws[3] + 1 == hws[4]
-        return low_ace_straight or straight
+def four_of_a_kind(hand):
+    hand_without_suits = get_cards_number_values(hand)
+    quads = find_duplicates(hand_without_suits, 4)
 
-    @staticmethod
-    def is_flush(suits):
-        return suits.count(suits[0]) == 5
+    if quads:
+        tie_breakers = create_tie_breaker_list(quads, hand_without_suits)
+        return Result('four_of_a_kind', hand, tie_breakers)
+    else:
+        return None
 
-    @staticmethod
-    def sort(a, b):
-        bigger_hand = PokerWinner.hand_value[a.type] > PokerWinner.hand_value[b.type]
-        same_hand = PokerWinner.hand_value[a.type] == PokerWinner.hand_value[b.type]
 
-        if same_hand:
-            bigger_tie_breaker = PokerWinner.compare_tie_breaker(a.tie_breaker, b.tie_breaker)
-            if bigger_tie_breaker:
-                return -1
-            else:
-                return 1
+def straight_flush(hand):
+    hws = sorted(get_cards_number_values(hand))
+    suits = sorted(get_cards_suits(hand))
 
-        if bigger_hand:
+    if is_straight(hws) and is_flush(suits):
+        # using number in middle of straight as a tie breaker to avoid low ace's
+        return Result('straight_flush', hand, [hws[2]])
+    else:
+        return None
+
+
+def create_tie_breaker_list(main_hand, full_hand):
+    main_hand.sort(reverse=True)
+    kickers = [x for x in full_hand if x not in main_hand]
+    kickers.sort(reverse=True)
+    main_hand.extend(kickers)
+    return main_hand
+
+
+def find_duplicates(numbers, number_of_dups):
+    duplicateNumbers = []
+    for x in card_values.values():
+        duplicateCount = numbers.count(x)
+        if duplicateCount == number_of_dups:
+            duplicateNumbers.append(x)
+    return duplicateNumbers
+
+
+def get_cards_number_values(hand):
+    return list(map(lambda x: card_values[x[:-1]], hand[1]))
+
+
+def get_cards_suits(hand):
+    return list(map(lambda x: x[len(x) - 1], hand[1]))
+
+
+def is_straight(hws):
+    low_ace_straight = hws[0] + 1 == hws[1] and hws[1] + 1 == hws[2] and hws[2] + 1 == hws[3] and hws[4] == 14
+    straight = hws[0] + 1 == hws[1] and hws[1] + 1 == hws[2] and hws[2] + 1 == hws[3] and hws[3] + 1 == hws[4]
+    return low_ace_straight or straight
+
+
+def is_flush(suits):
+    return suits.count(suits[0]) == 5
+
+
+def sort(a, b):
+    bigger_hand = hand_value[a.type] > hand_value[b.type]
+    same_hand = hand_value[a.type] == hand_value[b.type]
+
+    if same_hand:
+        bigger_tie_breaker = compare_tie_breaker(a.tie_breaker, b.tie_breaker)
+        if bigger_tie_breaker:
             return -1
         else:
             return 1
+
+    if bigger_hand:
+        return -1
+    else:
+        return 1
--- test/poker_winner_test.py
+++ test/poker_winner_test.py
@@ -1,121 +1,103 @@
 import unittest
-from poker.poker_winner import PokerWinner
+from poker.poker_winner import find_winning_hand
 
 
 class MyTest(unittest.TestCase):
 
     def test_high_card_1(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['AS', '2H', '3H', 'JC', '8D'])
         jons_hand = ('Jon', ['KS', '2H', '3H', 'JC', '8D'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Zak')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Zak')
 
     def test_high_card_2(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['KS', '2H', '3H', 'JC', '8D'])
         jons_hand = ('Jon', ['6S', '2H', '3H', 'JC', 'AS'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Jon')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Jon')
 
     def test_pair_beats_high_card(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['KS', '2H', '3H', 'JC', 'KH'])
         jons_hand = ('Jon', ['6S', '2H', '3H', 'JC', 'AS'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Zak')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Zak')
 
     def test_pair_beats_lower_pair(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['QS', '2H', 'AH', 'JC', 'QH'])
         jons_hand = ('Jon', ['KS', 'KH', '3H', 'JC', '8S'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Jon')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Jon')
 
     def test_two_pair_beats_pair(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['QS', '2H', '7H', '2D', 'QH'])
         jons_hand = ('Jon', ['KS', 'KH', '3H', 'AC', '8S'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Zak')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Zak')
 
     def test_two_pair_beats_lower_two_pair(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['QS', 'QH', '7H', '4D', '4H'])
         jons_hand = ('Jon', ['KS', 'KH', '3H', '3C', '8S'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Jon')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Jon')
 
     def test_three_of_a_kind_beats_two_pair(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['QS', 'QH', '7H', '4D', '4H'])
         jons_hand = ('Jon', ['KS', 'KH', 'KC', '3C', '8S'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Jon')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Jon')
 
     def test_three_of_a_kind_beats_lower_three_of_a_kind(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['QS', 'QH', 'QC', '2D', '4H'])
         jons_hand = ('Jon', ['KS', 'KH', 'KC', '3C', '8S'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Jon')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Jon')
 
     def test_straight_beats_three_of_a_kind(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['6S', '7H', '8C', '9D', '10H'])
         jons_hand = ('Jon', ['KS', 'KH', 'KC', '3C', '8S'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Zak')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Zak')
 
     def test_straight_ace_can_be_used_as_low(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['AS', '2H', '3C', '4D', '5H'])
         jons_hand = ('Jon', ['KS', 'KH', 'KC', '3C', '8S'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Zak')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Zak')
 
     def test_straight_can_beat_lower_straight(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['6S', '7H', '8C', '9D', '10H'])
         jons_hand = ('Jon', ['7S', '8H', '9C', '10C', 'JS'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Jon')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Jon')
 
     def test_flush_can_beat_a_straight(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['7S', '8H', '9C', '10C', 'JH'])
         jons_hand = ('Jon', ['5S', '9S', 'JS', '2S', 'AS'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Jon')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Jon')
 
     def test_flush_can_beat_a_smaller_flush(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['2H', '3H', '4H', '5H', 'AH'])
         jons_hand = ('Jon', ['5S', '9S', 'JS', '2S', 'KS'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Zak')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Zak')
 
     def test_full_house_beats_flush(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['2S', '3H', '4C', '5H', 'AH'])
         jons_hand = ('Jon', ['5S', '5S', '8S', '8S', '8S'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Jon')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Jon')
 
     def test_full_house_beats_lower_full_house(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['AS', 'AS', '7S', '7S', '7S'])
         jons_hand = ('Jon', ['5S', '5S', '8S', '8S', '8S'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Jon')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Jon')
 
     def test_four_of_a_kind_beats_full_house(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['AS', 'AS', '7S', '7S', '7S'])
         jons_hand = ('Jon', ['5S', '2S', '2C', '2D', '2H'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Jon')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Jon')
 
     def test_four_of_a_kind_beats_lower_four_of_a_kind(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['AS', '8S', '8C', '8D', '8H'])
         jons_hand = ('Jon', ['5S', '2S', '2C', '2D', '2H'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Zak')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Zak')
 
     def test_straight_flush_beats_quads(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['AS', '8S', '8C', '8D', '8H'])
         jons_hand = ('Jon', ['5S', '6S', '7S', '8S', '9S'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Jon')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Jon')
 
     def test_straight_flush_beats_lower_straight_flush(self):
-        subject = PokerWinner()
         zaks_hand = ('Zak', ['6S', '7S', '8S', '9S', '10S'])
         jons_hand = ('Jon', ['5S', '6S', '7S', '8S', '9S'])
-        self.assertEqual(PokerWinner.find_winning_hand(subject, [zaks_hand, jons_hand]), 'Zak')
+        self.assertEqual(find_winning_hand([zaks_hand, jons_hand]), 'Zak')
+
 
 
